a8de2c14586cf3456d23670dedba9f79f5fd4a0d
Vitor Mendes <mendes.v@gsuite.iff.edu.br>
2025-03-26 13:01:46 -0300

    created IClearTransactionsUseCase

---
+++ src/main/java/com/desafio/itau/springboot/usecases/ClearTransactionsUsecase.java
@@ -0,0 +1,18 @@
+package com.desafio.itau.springboot.usecases;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import com.desafio.itau.springboot.contracts.imodels.ITransaction;
+import com.desafio.itau.springboot.contracts.iusecase.IClearTransactionsUsecase;
+
+public class ClearTransactionsUsecase implements IClearTransactionsUsecase{
+
+    private final Queue<ITransaction> transactions = new ConcurrentLinkedQueue<>();
+
+    @Override
+    public void clearTransactions() {
+       transactions.clear();
+    }
+    
+}
