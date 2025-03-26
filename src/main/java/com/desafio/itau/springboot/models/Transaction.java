55a2ef256f25aad7e5f705b1f6b5fe83c0f7cb40
Vitor Mendes <mendes.v@gsuite.iff.edu.br>
2025-03-26 12:17:37 -0300

    created model Transaction

---
+++ src/main/java/com/desafio/itau/springboot/models/Transaction.java
@@ -0,0 +1,27 @@
+package com.desafio.itau.springboot.models;
+
+import java.time.OffsetDateTime;
+
+import com.desafio.itau.springboot.contracts.ITransaction;
+
+public class Transaction implements ITransaction {
+
+   
+        private double amount;
+        private OffsetDateTime date;
+    
+    
+        public void transaction(final double amount, final  OffsetDateTime date) {
+            this.amount = amount;
+            this.date = date;
+        }
+    
+        public double getAmount() {
+            return amount;
+        }
+    
+        public OffsetDateTime getDate() {
+            return date;
+        }
+    
+}
