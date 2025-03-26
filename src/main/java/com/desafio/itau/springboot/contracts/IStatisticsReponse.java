2f32f64647b2a620afd1958d2492e181b80da884
Vitor Mendes <mendes.v@gsuite.iff.edu.br>
2025-03-26 12:32:23 -0300

    created dto IStatisticsReponse

---
+++ src/main/java/com/desafio/itau/springboot/contracts/IStatisticsReponse.java
@@ -0,0 +1,9 @@
+package com.desafio.itau.springboot.contracts;
+
+public interface IStatisticsReponse {
+    double getSum();
+    double getAvg();
+    double getMax();
+    double getMin();
+    long getCount();
+}
